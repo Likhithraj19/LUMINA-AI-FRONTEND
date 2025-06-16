696b687fd1408b55d65fbf559f11bf412c6ab4ef
Likhithraj19 <likhithrj@gmail.com>
2025-06-16 16:08:32 +0530

    [NEW] Installed shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
-    <div className="flex flex-col items-center justify-center min-h-screen bg-indigo-950">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-indigo-950 text-gray-100 text-3xl ">
       Hello Test!
+      <Button className="mt-4 bg-indigo-600 hover:bg-indigo-700 text-white">
+        Click Me
+      </Button>
     </div>
     </>
   );
