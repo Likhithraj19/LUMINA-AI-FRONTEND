d2734574e1f771427476ab94e475e8ba9c57da53
Likhithraj19 <likhithrj@gmail.com>
2025-06-23 15:17:01 +0530

    [NEW] Demo Landing page Gradient text-hover-effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,10 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Input, Skeleton, Card } from "@heroui/react";
+import { TextHoverEffect } from "@/components/Animations/text-hover-effect";
 
 export default function Home() {
   return (
     <>
-    <div className="flex flex-col items-center justify-center min-h-screen bg-indigo-950 text-gray-100 text-3xl ">
-    <Card className="w-[200px] space-y-5 p-4" radius="lg">
-      <Skeleton className="rounded-lg">
-        <div className="h-24 rounded-lg bg-default-300" />
-      </Skeleton>
-      <div className="space-y-3">
-        <Skeleton className="w-3/5 rounded-lg">
-          <div className="h-3 w-3/5 rounded-lg bg-default-200" />
-        </Skeleton>
-        <Skeleton className="w-4/5 rounded-lg">
-          <div className="h-3 w-4/5 rounded-lg bg-default-200" />
-        </Skeleton>
-        <Skeleton className="w-2/5 rounded-lg">
-          <div className="h-3 w-2/5 rounded-lg bg-default-300" />
-        </Skeleton>
-      </div>
-    </Card>
-      <div className="mt-4 flex w-2xl flex-wrap md:flex-nowrap gap-4">
-      <Input label="Email" type="email" variant="bordered" />
-    </div>
+    <div className="h-[40rem] flex items-center justify-center">
+      <TextHoverEffect text="LUMINA" />
     </div>
     </>
   );
