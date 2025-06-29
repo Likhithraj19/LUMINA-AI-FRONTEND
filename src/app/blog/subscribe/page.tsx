b45397f3b9dfa4d11eebcf5eaf53db1b1b490c52
Likhithraj19 <likhithrj@gmail.com>
2025-06-29 12:28:50 +0530

    [NEW] added subsciption demo modal

--- src/app/blog/subscribe/page.tsx
+++
@@ -1,8 +0,0 @@
-import { SubscribeSection } from "@/components/subscribe/SubscribeSection";
-export default function SubscribePage() {
-    return (
-        <div>
-            <SubscribeSection />
-        </div>
-    )
-}
